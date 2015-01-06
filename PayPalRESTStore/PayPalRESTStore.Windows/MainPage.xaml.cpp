1bcb5418dc6805cd4e8205b9ab05614ef567e7ee
U-REDMOND\v-oniobi <v-oniobi@v-oniobi-1217.redmond.corp.microsoft.com>
2015-01-05 22:53:11 -0800

    First commit

---
+++ PayPalRESTStore/PayPalRESTStore.Windows/MainPage.xaml.cpp
@@ -0,0 +1,50 @@
+﻿//
+// MainPage.xaml.cpp
+// Implementation of the MainPage class.
+//
+
+#include "pch.h"
+#include "MainPage.xaml.h"
+
+
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+MainPage::MainPage()
+{
+	InitializeComponent();
+}
+
+
+void PayPalRESTStore::MainPage::Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e)
+{
+	String^ bufferString = CryptographicBuffer::EncodeToBase64String(CryptographicBuffer::ConvertStringToBinary(ref new String(L"AXVXPhBYtwEl_oJV1U8BohrSTFUG7A8IPOpYLaP3A_30cZ7L_w94r3G2gvsa:EJQ_ZhBDtIGpd1yGmtDsKyihcfcQ_9dzpHXjaTSVX39Li7zP91kiBkd8BiJk"), BinaryStringEncoding::Utf8));
+	Uri^ uri = ref new Uri(ref new String(L"https://api.sandbox.paypal.com/v1/oauth2/token"));
+	HttpRequestMessage^ request = ref new HttpRequestMessage(HttpMethod::Post, uri);
+	request->Headers->Append("Accept", "application/json");
+	request->Headers->Authorization = ref new HttpCredentialsHeaderValue("Basic", bufferString);
+	Map<String^, String^>^ iMap = ref new Map<String^, String^>();
+	iMap->Insert(ref new String(L"grant_type"), ref new String(L"client_credentials"));
+	iMap->Insert(ref new String(L"content-type"), ref new String(L"application/x-www-form-urlencoded"));
+	request->Content = ref new HttpFormUrlEncodedContent(iMap);
+	HttpClient^ httpClient = ref new HttpClient();
+	create_task(httpClient->SendRequestAsync(request)).then([this](HttpResponseMessage^ response)
+	{
+		JsonObject^ jsonObject = JsonObject::Parse(response->Content->ReadAsStringAsync()->GetResults());
+		String^ accessToken = jsonObject->GetNamedString("access_token", "");
+		Uri^ uri = ref new Uri(ref new String(L"https://api.sandbox.paypal.com/v1/identity/openidconnect/userinfo/?schema=openid"));
+		HttpRequestMessage^ request = ref new HttpRequestMessage(HttpMethod::Post, uri);
+		request->Headers->Authorization = ref new HttpCredentialsHeaderValue("Bearer", accessToken);
+		/*
+		Map<String^, String^>^ iMap = ref new Map<String^, String^>();
+		iMap->Insert(ref new String(L"content-type"), ref new String(L"application/json"));
+		String^ jsonContent = ref new String(L"{\"intent\":\"sale\"},\"payer\":{\"payment_method\":\"paypal\"},\"transactions\":[{\"amount\":{\"total\":\"1\",\"currency\":\"USD\"}}]}");
+		request->Content = ref new HttpStringContent(jsonContent);
+		*/
+		HttpClient^ httpClient = ref new HttpClient();
+		create_task(httpClient->SendRequestAsync(request)).then([this](HttpResponseMessage^ response)
+		{
+			_response->Text = JsonObject::Parse(response->Content->ReadAsStringAsync()->GetResults())->GetNamedString("user_id", "");
+		});
+	}, task_continuation_context::use_current());
+}
---
+++ PayPalRESTStore/PayPalRESTStore.Windows/MainPage.xaml.h
@@ -0,0 +1,43 @@
+﻿//
+// MainPage.xaml.h
+// Declaration of the MainPage class.
+//
+
+#pragma once
+
+#include "MainPage.g.h"
+
+using namespace Concurrency;
+using namespace Platform;
+using namespace Platform::Collections;
+using namespace Windows::Data::Json;
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+using namespace Windows::Security::Cryptography;
+using namespace Windows::Storage::Streams;
+using namespace Windows::UI::Xaml;
+using namespace Windows::UI::Xaml::Controls;
+using namespace Windows::UI::Xaml::Controls::Primitives;
+using namespace Windows::UI::Xaml::Data;
+using namespace Windows::UI::Xaml::Input;
+using namespace Windows::UI::Xaml::Media;
+using namespace Windows::UI::Xaml::Navigation;
+using namespace Windows::Web::Http;
+using namespace Windows::Web::Http::Headers;
+
+namespace PayPalRESTStore
+{
+	/// <summary>
+	/// An empty page that can be used on its own or navigated to within a Frame.
+	/// </summary>
+	public ref class MainPage sealed
+	{
+	public:
+		MainPage();
+
+	private:
+		void Button_Click(Platform::Object^ sender, Windows::UI::Xaml::RoutedEventArgs^ e);
+	};
+}
+
+using namespace PayPalRESTStore;
